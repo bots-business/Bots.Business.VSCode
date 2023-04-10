9f23f6beec89a53ce25a21d10bfdfbec27a15de9
Bots.Business <git.sync@bots.business>
2023-04-10 14:08:28 +0700

    code refactoring

--- src/tree/drop_handler.ts
+++ src/tree/drop_handler.ts
@@ -15,62 +15,65 @@ function isAllowed(target: any){
   return true;
 }
 
+function showAlertCantTransferBeetwenBots(){
+  vscode.window.showErrorMessage(
+    "Can't Transfer Command from one Bot to another Bot",
+    { modal: true }
+  );
+}
+
+function dropToCommandTree(target: any, bbCommand: any){
+  if(!(target instanceof CommandTree)) { return; }
+  if (target.parent.bot.id !== bbCommand.bot_id) {
+    showAlertCantTransferBeetwenBots();
+    return;
+  }
+  if (!bbCommand.commands_folder_id) {
+    //it was not in any folder before
+    //so it was trying to transfer to same place
+    return;
+  }
+  return true;
+}
+
+function dropToFolderTree(target: any, bbCommand: any){
+  if(!(target instanceof FolderTreeItem)) { return; }
+  if (target.parent.parent.bot.id !== bbCommand.bot_id) {
+    showAlertCantTransferBeetwenBots();
+    return;
+  }
+  if (target.folder.id === bbCommand.commands_folder_id) {
+    //it was trying to transfer to same folder
+    return;
+  }
+  return true;
+}
+
 export async function dropHandler(target: any, bbCommand: any) {
-  let warningStatement;
   if (target instanceof CommandTreeItem) {
     target = target.parent;
   }
 
   if(!isAllowed(target)){ return; }
+  let warningStatement;
 
-
-  if (target instanceof CommandTree) {
-    if (target.parent.bot.id !== bbCommand.bot_id) {
-      vscode.window.showErrorMessage(
-        "Can't Transfer Command from one Bot to another Bot",
-        { modal: true }
-      );
-      return;
-    }
-
-    //its same bot now
-
-    if (!bbCommand.commands_folder_id) {
-      //it was not in any folder before
-      //so it was trying to transfer to same place
-      return;
-    }
-
+  if(dropToCommandTree(target, bbCommand)){
     //now the remaining condition is command should come from any folder to no folder
     bbCommand.commands_folder_id = null;
-    warningStatement = `Do u want to move Command: ${bbCommand.command} to root Folder`;
+    warningStatement = " to root Folder";
   }
-
-  if (target instanceof FolderTreeItem) {
-    if (target.parent.parent.bot.id !== bbCommand.bot_id) {
-      vscode.window.showErrorMessage(
-        "Cant Transfer Command from One Bot to Another Bot",
-        { modal: true }
-      );
-      return;
-    }
-
-    //its same bot now
-
-    if (target.folder.id === bbCommand.commands_folder_id) {
-      //it was trying to transfer to same folder
-      return;
-    }
-
+  
+  else if(dropToFolderTree(target, bbCommand)){
     //now remaining condition is commannd comes from no folder/any other to the target folder
     bbCommand.commands_folder_id = target.folder.id;
-    warningStatement = `Do u want to move Command: ${bbCommand.command} to ${target.folder.title} Folder`;
-  }
-  if (!warningStatement) {
-    return;
+    warningStatement = " to ${target.folder.title} Folder";
   }
 
-  const confirmed = await showConfirmationDialog(warningStatement);
+  else { return; }
+
+  const confirmed = await showConfirmationDialog(
+    `Do u want to move Command: ${bbCommand.command} ` + warningStatement
+  );
 
   if (!confirmed) {
     return;
