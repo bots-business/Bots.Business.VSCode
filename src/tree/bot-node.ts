f7480c3e253bf8dc0ee05af4576c12286a86f452
Bots.Business <git.sync@bots.business>
2023-04-10 13:50:14 +0700

    code refactoring

--- src/tree/bot-node.ts
+++ src/tree/bot-node.ts
@@ -46,25 +46,12 @@ export class BotNode extends vscode.TreeItem {
 }
 
 export function getBotNode(element: MenuItemTypes) {
-  if (element instanceof BotNode) {
-    return element;
-  }
-  if ((element instanceof LibTree)||
-      (element instanceof CommandTree)||
-      (element instanceof ErrorTree)) {
-    return element.parent;
-  }
-  if ((element instanceof LibTreeItem)||
-     (element instanceof CommandTreeItem)) {
-    return element.parent.parent;
-  }
-  const isCommandTreeItem = (element instanceof CommandTreeItem);
-  if (!isCommandTreeItem) { return; }
-  if (element.parent instanceof FolderTreeItem) {
-    return element.parent.parent.parent;
-  }
-  if (element.parent instanceof CommandTree) {
-    return element.parent.parent;
+  for (let i = 0; i < 4; i++) {
+    if(element instanceof BotNode) {
+      return element;
+    }
+    element = element.parent;
+    // deep to element.parent.parent.parent
   }
 }
 
